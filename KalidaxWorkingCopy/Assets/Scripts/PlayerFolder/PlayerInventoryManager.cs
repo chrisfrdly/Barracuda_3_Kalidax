d2374472d388456995e6e572a1bf1be86367e7b5
chrisfrdly <101155734+chrisfrdly@users.noreply.github.com>
2023-11-14 16:51:41 -0500

    Picking up Items
    Whats New:
    - You can pick up items that have the Item Monobehavior script attached.
    - Items are put into the inventory during runtime and are taken out of it when you quit the application

--- KalidaxWorkingCopy/Assets/Scripts/PlayerFolder/PlayerInventoryManager.cs
+++ KalidaxWorkingCopy/Assets/Scripts/PlayerFolder/PlayerInventoryManager.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerInventoryManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public SO_Inventory inventory;
+
+    //will pick up any item that is on the ground and add it to the inventory 
+    public void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        var item = collision.GetComponent<Item>();
+        //checks if this item does not return null
+        if(item)
+        {
+            inventory.AddItem(item.item, 1);
+            Destroy(collision.gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    //this resets the inventory when you stop playing the game
+    private void OnApplicationQuit()
     {
-        
+        inventory.container.Clear();
     }
 }
