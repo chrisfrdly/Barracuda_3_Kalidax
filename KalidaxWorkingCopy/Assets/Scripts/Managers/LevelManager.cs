f137d5185fed4f0fefa814547070ffb19e639735
cerohaven <amckenzie09@hotmail.com>
2024-02-06 23:31:45 -0500

    Scene implementation done

---
+++ KalidaxWorkingCopy/Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadPlay(int sceneBuildIndex)
+    {
+        SceneManager.LoadScene("MainScene-Playtest 1");
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene("About");
+    }
+
+    public void LoadTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+}
